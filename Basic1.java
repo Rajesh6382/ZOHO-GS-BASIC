61b24b989d44d0d10b5a3869021fdfe4a7e2c2dc
Rajesh6382 <rajeshmurugan8056@gmail.com>
2025-06-28 11:01:13 +0530

    commit

--- Basic1.java
+++ Basic1.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 //<<<<<<<<<<<<  41-43 Patterns  >>>>>>>>>>>>//
@@ -289,25 +292,323 @@ class sol{
     }
 }
 
-class sol1{
-    public static void main(String[] args) {
-        int [] arr = {12,17,15,13,10,11,12};
-        int max=0;
-        int start=0;
-        int end=0;
-        int sum=0;
-        while(end<arr.length){
-            if(arr[start]<=arr[end]){
-                sum = sum+arr[end];
-                end++;
-            }else{
-                start=end;
-                sum=0;
+class Prog51
+{
+    public static void main(String[]args)
+    {
+        int[]nums = {1,6,2,5,3,4};
+        List<Integer>odd = new ArrayList<>();
+        List<Integer>even = new ArrayList<>();
+        for(int i = 0;i<nums.length;i++)
+        {
+            if(i%2==0)
+            {
+                odd.add(nums[i]);
             }
-            if(max<sum){
-                max=sum;
+            else
+            {
+                even.add(nums[i]);
             }
         }
-        System.out.println(max);
+        Collections.sort(odd, Collections.reverseOrder());
+        Collections.sort(even);
+        int j = 0, k = 0;
+        for(int i = 0;i<nums.length;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(odd.get(j++) + " ");
+            }
+            else
+            {
+                System.out.print(even.get(k++)+ " ");
+            }
+        }
+    }
+}
+
+//Program to print the alphabet "C" based on the given numbers
+class Prog52
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int n = java.nextInt();
+        for(int i = 1;i<=n;i++)
+        {
+            if(i==1 || i==n)
+            {
+               for(int j = 1;j<=n;j++)
+               {
+                 System.out.print("* ");
+               }
+            }
+            else
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+//Program to print a pyramid with numbers increased by 1
+class Prog53
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = java.nextInt();
+        int count = 1;
+            for(int i = 1;i<=n;i++)
+            {
+                for(int j = 1;j<=n;j++)
+                {
+                if(j<=(n-i))
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print(count++ + " ");
+                }
+                }
+                System.out.println("");
+            }
+    }
+}
+
+/*Program to print the given string in an "x" format
+1   5
+ 2 4 
+  3  
+ 2 4 
+1   5 */
+class Prog54
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter the n value: ");
+        int n = java.nextInt();
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if(i==j || i+j==n+1)
+                {
+                    System.out.print(j);
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
+
+/*Program to print the following pattern
+4444444
+4333334
+4322234
+4321234
+4322234
+4333334
+4444444 */
+class Prog55
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter the n value: ");
+        int n = java.nextInt();
+        int mid = n/2;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                int val = Math.max(Math.abs(i - mid), Math.abs(j - mid)) + 1;
+                System.out.print(val);
+            }
+            System.out.println("");
+
+        }
+    }
+}
+
+//Program to print all the substring of the given string without using strings
+class Prog56
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter any string: ");
+        String str = java.nextLine();
+        char[]ch = str.toCharArray();
+        for(int i = 0;i<ch.length;i++)
+        {
+            for(int j = i;j<ch.length;j++)
+            {
+                for(int k = i;k<=j;k++)
+                {
+                    System.out.print(ch[k]);
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
+
+//Print the numbers which are mismatched from two array
+class Prog57
+{
+    public static void main(String[]args)
+    {
+        char[]ch1 = {'a','b','c','d','e','f','g','h','i'};
+        char[]ch2 = {'a','b','d','e','e','g','g','i','i'};
+        for(int i = 0;i<ch1.length;i++)
+        {
+            if(ch1[i] == ch2[i])
+            {
+                continue;
+            }
+            System.out.println(ch1[i]+""+ch2[i]);
+        }
+    }
+}
+
+//Program to print the sum of 2 huge numbers as separate digits, store them in array and process the result
+class Prog58
+{
+    public static void main(String[]args)
+    {
+        int[]a = {9,2,8,1,3,5,6,7,3,1,1,6};
+        int[]b = {7,8,4,6,2,1,9,9,7};
+        int al = a.length - 1;
+        int bl = b.length - 1;
+        int[]c = new int[(Math.max(al,bl) + 1)];
+        int cl = c.length - 1;
+        int temp = 0;
+        while(al>=0||bl>=0||temp!=0)
+        {
+            int num1 = (al>=0)?a[al]:0;
+            int num2 = (bl>=0)?b[bl]:0;
+            c[cl] = (num1 + num2 + temp)%10;
+            temp = (num1 + num2 + temp)/10;
+            al--;
+            bl--;
+            cl--;
+        }
+        int start = 0;
+        if(c[start] == 0)
+        {
+            start = 1;
+        }
+        for(int i = start;i<c.length;i++)
+        {
+            System.out.print(c[i]+ " ");
+        }
+    }
+}
+
+/*Program to print the below format
++===+===+
+| | |
+| | |
+| | |
++===+===+
+| | |
+| | |
+| | |
++===+===+*/
+class Prog59
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = java.nextInt();
+        for(int i = 0;i<=n;i++)
+        {
+            if(i%4==0)
+            {
+                System.out.println("+===+===+");
+            }
+            else
+            {
+                System.out.println("| | |");
+            }
+        }
+    }
+}
+
+/*Program  to save the string "WELCOMETOZOHOCORPORATION" in a two dimensional array and search for substring
+"too" in the two dimensional string both from left to right and from top to bottom
+W E L C O
+M E T O Z
+O H O C O
+R P O R A
+T I O N */
+class Prog60
+{
+    public static void main(String[]args)
+    {
+        Scanner java = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = java.nextLine();
+        str = str.replaceAll(" ", "");
+
+        int ind = 0;
+        int size = (int)Math.ceil(Math.sqrt(str.length()));
+        char[][]mat = new char[size][size];
+
+        for(int i = 0;i<mat.length && ind<str.length();i++)
+        {
+            for(int j = 0;j<mat[0].length && ind<str.length();j++)
+            {
+                mat[i][j] = str.charAt(ind++);
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Enter the word to search: ");
+        String word = java.nextLine();
+
+        //for the row search
+        for(int i = 0;i<mat.length;i++)
+        {
+            StringBuilder rowchk = new StringBuilder();
+            for(int j = 0;j<mat[0].length;j++)
+            {
+                rowchk.append(mat[i][j]);
+            }
+            if(rowchk.toString().contains(word))
+            {
+                int pos = rowchk.indexOf(word);
+                System.out.println("The word present from ("+ i + ", " + pos +") to ("+ i + ", " + (pos + word.length() -1) + ") in the row");
+                break;
+            }
+        }
+
+        //for the column search
+        for(int j = 0;j<mat[0].length;j++)
+        {
+            StringBuilder colchk = new StringBuilder();
+            for(int i = 0;i<mat.length;i++)
+            {
+                colchk.append(mat[i][j]);
+            }
+            if(colchk.toString().contains(word))
+            {
+                int pos = colchk.indexOf(word);
+                System.out.println("The word present from (" + pos + ", " + j +") to (" + (pos + word.length() -1) + ", " + j + ") in the column");
+                break;
+            }
+        }
     }
 }
